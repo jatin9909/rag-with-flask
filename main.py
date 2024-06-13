573134b67b1e6910e9666638785634130774df45
Jatin <jatin.dhar27@gmail.com>
2024-06-13 16:45:11 +0530

    Added decomposition functionality

--- main.py
+++ main.py
@@ -149,7 +149,74 @@ def multiquery():
 
     return response_answer
 
+@app.route("/decompsoition", methods=["POST"])
+def create_question():
+    json_content = request.json
+    query = json_content.get("query")
 
+    vectorstore = Chroma(persist_directory=folder_path, embedding_function=embeddings)
+    retriever = vectorstore.as_retriever() # for similarity search
+
+    # Decomposition
+    template_3 = """You are a helpful assistant that generates multiple sub-questions related to an input question. \n
+    The goal is to break down the input into a set of sub-problems / sub-questions that can be answers in isolation. \n
+    Generate multiple search queries related to: {question} and nothing else\n
+    Output (3 queries):"""
+    prompt_decomposition = ChatPromptTemplate.from_template(template_3)
+    # Chain
+    generate_queries_decomposition = ( prompt_decomposition | cached_llm | StrOutputParser() | (lambda x: x.split("\n")))
+
+    # Run
+    # question = "I dont understand RAG ,Can you help me understand what are the components and one more thing I would like to know about whether is it same as Advanced RAG ?"
+    #### I gave an ambigious query which talks about 3 questions 1. RAG understanding 2. Components of RAG 3. Difference between RAG & Advanced RAG
+    questions = generate_queries_decomposition.invoke({"question":query})
+
+    print("questions- ", questions)
+
+    template_4 = """Here is the question you need to answer:
+
+        \n --- \n {question} \n --- \n
+
+        Here is any available background question + answer pairs:
+
+        \n --- \n {q_a_pairs} \n --- \n
+
+        Here is additional context relevant to the question: 
+
+        \n --- \n {context} \n --- \n
+
+        Use the above context and any background question + answer pairs to answer the question: \n {question}
+        """
+
+    decomposition_prompt = ChatPromptTemplate.from_template(template_4)
+
+    def format_qa_pair(question, answer):
+    #"""Format Q and A pair"""
+    
+        formatted_string = ""
+        formatted_string += f"Question: {question}\nAnswer: {answer}\n\n"
+        return formatted_string.strip()
+
+
+
+    q_a_pairs = ""
+    for q in questions:
+
+        rag_chain = (
+        {"context": itemgetter("question") | retriever, 
+        "question": itemgetter("question"),
+        "q_a_pairs": itemgetter("q_a_pairs")} 
+        | decomposition_prompt
+        | cached_llm
+        | StrOutputParser())
+
+        answer = rag_chain.invoke({"question":q,"q_a_pairs":q_a_pairs})
+        q_a_pair = format_qa_pair(q,answer)
+        q_a_pairs = q_a_pairs + "\n---\n"+  q_a_pair
+        
+    response_answer = {"Questions":questions, "answer":answer, "q_a_pair":q_a_pair, "q_a_pairs":q_a_pairs}
+
+    return response_answer 
 
 
 @app.route("/pdf", methods=["post"])
